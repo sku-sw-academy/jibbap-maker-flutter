5e8154e73946a6ab576ecde8b33991365336f36f
junhyunlim73 <limjh0703@gmail.com>
2024-05-23 11:37:11 +0900

    modfify detail

--- lib/mobile/home/detail.dart
+++ lib/mobile/home/detail.dart
@@ -25,7 +25,11 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("상세페이지"),),
+      appBar: AppBar(
+        title: Text("상세페이지"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
       body: FutureBuilder<List<PriceDTO>>(
         future: _data,
         builder: (context, snapshot) {
@@ -40,31 +44,68 @@ class _DetailPageState extends State<DetailPage> {
             return SingleChildScrollView(
               scrollDirection: Axis.horizontal, // 가로 스크롤을 위해 설정
               child: SingleChildScrollView(
-                child: DataTable(
-                  headingRowColor:
-                  MaterialStateColor.resolveWith((states) => Colors.blue),
-                  columnSpacing: 10,
-                  columns: [
-                    DataColumn(label: Text('품목')),
-                    DataColumn(label: Text('품종')),
-                    DataColumn(label: Text('단위')),
-                    DataColumn(label: Text('등급')),
-                    DataColumn(label: Text('당일가격')),
-                    DataColumn(label: Text('등락률')),
-                  ],
-                  rows: prices.map((price) {
-                    return DataRow(cells: [
-                      DataCell(Text(price.itemCode.itemName)),
-                      DataCell(Text(price.kindName)),
-                      DataCell(Text(price.unit)),
-                      DataCell(Text(price.rankName)),
-                      DataCell(Text(price.dpr1)),
-                      DataCell(Text(price.value.toString())),
-                      // Add more cells as needed
-                    ]);
-                  }).toList(),
-                ),
-              ),
+                    child: DataTable(
+                      headingRowColor:
+                      MaterialStateColor.resolveWith((states) => Colors.greenAccent),
+                      border: TableBorder.all(
+                        width: 3.0,
+                        color:Colors.black12,
+                      ),
+                      columns: [
+                        DataColumn(
+                            label:
+                          Expanded(
+                            child: Text('품목', textAlign:TextAlign.center)
+                          )
+                        ),
+
+                        DataColumn(label: Expanded(
+                            child:
+                            Text('품종', textAlign:TextAlign.center))
+                        ),
+
+                        DataColumn(label: Expanded(
+                            child:
+                            Text('단위', textAlign:TextAlign.center))
+                        ),
+
+                        DataColumn(label: Expanded(
+                            child:
+                            Text('등급', textAlign:TextAlign.center))
+                        ),
+
+                        DataColumn(label: Expanded(
+                            child:
+                            Text('당일가격', textAlign:TextAlign.center))
+                        ),
+
+                        DataColumn(label: Expanded(
+                            child:
+                            Text('등락률', textAlign:TextAlign.center))
+                        ),
+
+                        DataColumn(label: Expanded(
+                            child:
+                            Text('날짜', textAlign:TextAlign.center))
+                        ),
+
+                      ],
+                      rows: prices.map((price) {
+                        return DataRow(cells: [
+                          DataCell(Text(price.itemCode.itemName, textAlign:TextAlign.center)),
+                          DataCell(Text(price.kindName, textAlign:TextAlign.center)),
+                          DataCell(Text(price.unit, textAlign:TextAlign.center)),
+                          DataCell(Text(price.rankName, textAlign:TextAlign.center)),
+                          DataCell(Text(price.dpr1, textAlign:TextAlign.center)),
+                          DataCell(Text(price.value.toString(), textAlign:TextAlign.center)),
+                          DataCell(Text(price.regday, textAlign:TextAlign.center)),
+                          // Add more cells as needed
+                        ]
+                        );
+                      }
+                      ).toList(),
+                    ),
+                  ),
             );
           }
         },

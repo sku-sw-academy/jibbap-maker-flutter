c8379afa028a77241db145a49782c83ebd998ea7
junhyunlim73 <limjh0703@gmail.com>
2024-06-07 11:16:32 +0900

    modify faq, notice

--- lib/mobile/mypage/customerCenter/faq.dart
+++ lib/mobile/mypage/customerCenter/faq.dart
@@ -1,25 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_splim/constant.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class FAQPage extends StatefulWidget{
+class FAQPage extends StatefulWidget {
   @override
   _FAQPageState createState() => _FAQPageState();
 }
 
 class _FAQPageState extends State<FAQPage> {
+  List<Map<String, dynamic>> faqs = []; // FAQ 목록을 저장하는 리스트
+  bool isLoading = true; // 로딩 상태를 저장하는 변수
 
   @override
   void initState() {
     super.initState();
+    _fetchFAQs();
+  }
+
+  Future<void> _fetchFAQs() async {
+    try {
+      final url = Uri.parse('${Constants.baseUrl}/faq/all');
+      final response = await http.get(url, headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      });
+
+      if (response.statusCode == 200) {
+        var responseBody = utf8.decode(response.bodyBytes);
+        List<dynamic> faqList = json.decode(responseBody);
+        setState(() {
+          faqs = faqList.map((faq) {
+            var faqMap = faq as Map<String, dynamic>;
+            faqMap['isExpanded'] = false; // Initialize isExpanded to false
+            return faqMap;
+          }).toList();
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load FAQs');
+      }
+    } catch (e) {
+      print('Error fetching FAQs: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("FAQ 목록을 불러오는 데 실패했습니다."),
+        ),
+      );
+      setState(() {
+        isLoading = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-      body: Center(
-        child: Text("자주 하는 질문"),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: faqs.length,
+        itemBuilder: (context, index) {
+          var faq = faqs[index];
+          return ExpansionTile(
+            title: Text(faq['title'] , style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+            children: [
+              ListTile(
+                title: Text(" "+faq['content'], style: TextStyle(fontSize: 16)),
+              ),
+            ],
+            onExpansionChanged: (bool isExpanded) {
+              setState(() {
+                faq['isExpanded'] = isExpanded;
+              });
+            },
+            initiallyExpanded: faq['isExpanded'] ?? false,
+          );
+        },
       ),
     );
   }
-
 }
--- lib/mobile/mypage/customerCenter/notice.dart
+++ lib/mobile/mypage/customerCenter/notice.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_splim/constant.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class NoticePage extends StatefulWidget{
   @override
@@ -6,18 +9,73 @@ class NoticePage extends StatefulWidget{
 }
 
 class _NoticePageState extends State<NoticePage> {
+  List<Map<String, dynamic>> notices = []; // FAQ 목록을 저장하는 리스트
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
+    _fetchNotices();
+  }
+
+  Future<void> _fetchNotices() async {
+    try {
+      final url = Uri.parse('${Constants.baseUrl}/notice/all');
+      final response = await http.get(url, headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      });
+
+      if (response.statusCode == 200) {
+        var responseBody = utf8.decode(response.bodyBytes);
+        List<dynamic> noticeList = json.decode(responseBody);
+        setState(() {
+          notices = noticeList.map((notice) {
+            var noticeMap = notice as Map<String, dynamic>;
+            noticeMap['isExpanded'] = false; // Initialize isExpanded to false
+            return noticeMap;
+          }).toList();
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load Notices');
+      }
+    } catch (e) {
+      print('Error fetching Notices: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("공지사항 목록을 불러오는 데 실패했습니다."),
+        ),
+      );
+      setState(() {
+        isLoading = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-      body: Center(
-        child: Text("Notice"),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: notices.length,
+        itemBuilder: (context, index) {
+          var notice = notices[index];
+          return ExpansionTile(
+            title: Text(notice['title'] , style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+            children: [
+              ListTile(
+                title: Text(" "+notice['content'], style: TextStyle(fontSize: 16)),
+              ),
+            ],
+            onExpansionChanged: (bool isExpanded) {
+              setState(() {
+                notice['isExpanded'] = isExpanded;
+              });
+            },
+            initiallyExpanded: notice['isExpanded'] ?? false,
+          );
+        },
       ),
     );
   }
